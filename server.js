8d2e470b33fe5905f2cd5c72080419597a1512d2
Tracy Kubert <tracykubert@tracys-mbp.lan>
2017-07-11 22:45:53 -0400

    Attempted handlebars

--- server.js
+++ server.js
@@ -1,31 +1,38 @@
-//dependencies
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require("path");
 
+// Set-up port
 var PORT = process.env.PORT || 3000;
+// Set up variable to call express module
 var app = express();
 
 // Requiring our models for syncing
 var db = require("./models");
 
-//serves the public directory for access
+// Serves the public directory for access
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
+// Set Handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them.
 var routes = require("./controller/comic_controller.js");
 
 app.use("/", routes);
 
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
